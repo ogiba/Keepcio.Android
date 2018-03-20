6d2e2f4a048939f6bd072f9f6dca10c24661892b
Robert Ogiba <ogiback@gmail.com>
2018-03-20 18:14:36 +0100

    Provide method that allow determine whether user is logged in

--- app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginActivity.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginActivity.kt
@@ -1,13 +1,15 @@
 package pl.ogiba.keepcio.scenes.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import pl.ogiba.keepcio.R
+import pl.ogiba.keepcio.scenes.main.MainActivity
 
 class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
@@ -20,6 +22,14 @@ class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (checkIfUserLoggedIn()) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            return
+        }
+
         setContentView(R.layout.activity_login)
 
         bindViews()
@@ -55,5 +65,9 @@ class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     private fun bindListeners() {
         loginBtn?.setOnClickListener(this)
+    }
+
+    private fun checkIfUserLoggedIn(): Boolean {
+        return FirebaseAuth.getInstance().currentUser !== null
     }
 }
--- app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginPresenter.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginPresenter.kt
@@ -1,10 +1,14 @@
 package pl.ogiba.keepcio.scenes.login
 
+import com.google.firebase.auth.FirebaseAuth
+
 /**
  * Created by robertogiba on 19.03.2018.
  */
 class LoginPresenter : ILoginPresenter {
     private lateinit var loginView: ILoginView
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
     override fun subscribe(view: ILoginView) {
         this.loginView = view
