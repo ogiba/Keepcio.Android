c862787db0921271f2512c6181748fd88abd6299
Robert Ogiba <ogiback@gmail.com>
2018-03-21 16:41:54 +0100

    Provide simple login firebase login mechanism

--- app/src/main/java/pl/ogiba/keepcio/scenes/login/ILoginPresenter.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/ILoginPresenter.kt
@@ -6,4 +6,5 @@ import pl.ogiba.keepcio.utils.IBasePresenter
  * Created by robertogiba on 19.03.2018.
  */
 interface ILoginPresenter : IBasePresenter<ILoginView> {
+    fun loginUser(userName: String, password: String)
 }
--- app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginPresenter.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginPresenter.kt
@@ -1,11 +1,18 @@
 package pl.ogiba.keepcio.scenes.login
 
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+
 
 /**
  * Created by robertogiba on 19.03.2018.
  */
-class LoginPresenter : ILoginPresenter {
+class LoginPresenter : ILoginPresenter, FirebaseAuth.AuthStateListener, OnCompleteListener<AuthResult> {
+    private val TAG = LoginPresenter::class.simpleName
+
     private lateinit var loginView: ILoginView
 
     private val firebaseAuth = FirebaseAuth.getInstance()
@@ -14,5 +21,37 @@ class LoginPresenter : ILoginPresenter {
         this.loginView = view
 
         this.loginView.onSubscribe()
+    }
+
+    override fun loginUser(userName: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(userName, password).addOnCompleteListener(this)
+    }
+
+    override fun onComplete(task: Task<AuthResult>) {
+        val isSuccessful = task.isSuccessful
+        Log.d(TAG, "signInWithEmail:onComplete:" + isSuccessful)
+
+        if (isSuccessful) {
+            val user = firebaseAuth.currentUser
+        } else {
+            val taskException = task.exception
+            Log.w(TAG, "signInWithEmail:failed", taskException)
+
+//            if (taskException != null) {
+//                loginView.onLoginFailed(R.string.connection_problem_message)
+//            } else {
+//                loginView.onLoginFailed(R.string.activity_login_auth_failed)
+//            }
+        }
+
+    }
+
+    override fun onAuthStateChanged(auth: FirebaseAuth) {
+        val user = auth.currentUser
+        if (user != null) {
+            Log.d(TAG, "onAuthStateChanged:signed_in:" + user.uid)
+        } else {
+            Log.d(TAG, "onAuthStateChanged:signed_out")
+        }
     }
 }
