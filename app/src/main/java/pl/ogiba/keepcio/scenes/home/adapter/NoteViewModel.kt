f47c9cc457d13b83253a07b743d9f40c3d3ea04d
Robert Ogiba <ogiback@gmail.com>
2018-11-22 22:33:04 +0100

    Add simple click action to single row view

--- app/src/main/java/pl/ogiba/keepcio/scenes/home/adapter/NoteViewModel.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/home/adapter/NoteViewModel.kt
@@ -1,6 +1,7 @@
 package pl.ogiba.keepcio.scenes.home.adapter
 
 import android.content.Context
+import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import pl.ogiba.keepcio.models.Note
@@ -9,4 +10,8 @@ class NoteViewModel(val context: Context,
                     val note: Note) : ViewModel() {
 
     val title = ObservableField<String>(note.title)
+
+    fun navigateToNote() {
+        Log.d("Test", "NoteTitle: ${note.title}")
+    }
 }
