a268266b63401f836694f19c4b662f471e9b5366
Robert Ogiba <ogiback@gmail.com>
2018-12-09 00:18:53 +0100

    Remove LoginPresenter class replaced by LoginViewModel after moving from MVP to MVVM

--- app/src/main/java/pl/ogiba/keepcio/scenes/login/ILoginView.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/ILoginView.kt
@@ -9,15 +9,5 @@ import pl.ogiba.keepcio.utils.IBaseView
  * Created by robertogiba on 19.03.2018.
  */
 interface ILoginView : IBaseView {
-    fun onLoginUser()
 
-    fun onLoginFailed(@StringRes stringId: Int)
-
-    fun onValidationError(type: LoginErrorType, @StringRes stringId: Int)
-
-    fun onStateChange(state: LoginViewState)
-
-    fun onRegistrationStarted()
-
-    fun onPasswordNotMatch()
 }
--- app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginActivity.kt
+++ app/src/main/java/pl/ogiba/keepcio/scenes/login/LoginActivity.kt
@@ -17,10 +17,9 @@ import pl.ogiba.keepcio.scenes.login.utils.LoginViewState
 import pl.ogiba.keepcio.scenes.main.MainActivity
 
 
-class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
+class LoginActivity : AppCompatActivity(), ILoginView {
     private val TAG = "LoginActivity"
 
-    private lateinit var presenter: ILoginPresenter
     private lateinit var binding: ActivityLoginBinding
     private lateinit var viewModel: LoginViewModel
 
@@ -40,72 +39,6 @@ class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     override fun onSubscribe() {
         Log.d(TAG, "View subscribed")
-    }
-
-    override fun onLoginUser() {
-        changeViewState(false)
-
-        navigateToMainActivity()
-    }
-
-    override fun onLoginFailed(stringId: Int) {
-        changeViewState(false)
-
-        tv_error_box.visibility = View.VISIBLE
-        tv_error_box.text = resources.getString(stringId)
-    }
-
-    override fun onValidationError(type: LoginErrorType, stringId: Int) {
-        when (type) {
-            LoginErrorType.EMAIL -> {
-                val errorMessage = resources.getString(R.string.activity_login_username_required,
-                        resources.getString(stringId))
-                et_user_login.error = errorMessage
-            }
-            LoginErrorType.PASSWORD -> {
-                val errorMessage = resources.getString(R.string.activity_login_password_required,
-                        resources.getString(stringId))
-                et_user_password.error = errorMessage
-            }
-            LoginErrorType.REPASSWORD -> {
-                val errorMessage = resources.getString(R.string.activity_login_repeat_password_required,
-                        resources.getString(stringId))
-                et_user_repeat_password.error = errorMessage
-            }
-        }
-    }
-
-    override fun onStateChange(state: LoginViewState) {
-        if (state == LoginViewState.REGISTER) {
-            et_user_repeat_password.visibility = View.VISIBLE
-            btn_login.text = resources.getString(R.string.activity_login_btn_register_label)
-            tv_register_now.text = resources.getString(R.string.activity_login_back_to_login)
-        } else {
-            et_user_repeat_password.visibility = View.GONE
-            btn_login.text = resources.getString(R.string.activity_login_btn_login_label)
-            tv_register_now.text = resources.getString(R.string.activity_login_register_now_label)
-        }
-    }
-
-    override fun onRegistrationStarted() = performRegisterAction()
-
-    override fun onPasswordNotMatch() {
-        et_user_password.error = resources.getString(R.string.activity_login_different_password)
-        et_user_repeat_password.error = resources.getString(R.string.activity_login_different_password)
-    }
-
-    override fun onClick(v: View?) {
-        if (v == null) {
-            return
-        }
-
-        when (v.id) {
-            R.id.btn_login -> performLoginAction()
-            R.id.tv_register_now -> presenter.changeState()
-            else -> {
-                print("Click event not supported for: " + v.id)
-            }
-        }
     }
 
     private fun bindListeners() {
@@ -164,44 +97,10 @@ class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     private fun checkIfUserLoggedIn(): Boolean = FirebaseAuth.getInstance().currentUser !== null
 
-    private fun performLoginAction() {
-        changeViewState()
-
-        val text = et_user_login.text.toString()
-        val password = et_user_password.text.toString()
-
-        presenter.loginUser(text, password)
-    }
-
-    private fun performRegisterAction() {
-        changeViewState()
-
-        val text = et_user_login.text.toString()
-        val pw = et_user_password.text.toString()
-        val repeatedPw = et_user_repeat_password.text.toString()
-
-        presenter.registerUser(text, pw, repeatedPw)
-    }
-
     private fun navigateToMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         startActivity(intent)
         finish()
-    }
-
-    private fun changeViewState(inProgress: Boolean = true) {
-        this.progress_bar.visibility = if (inProgress) View.VISIBLE else View.GONE
-        this.et_user_password.isEnabled = !inProgress
-        this.et_user_repeat_password.isEnabled = !inProgress
-        this.et_user_login.isEnabled = !inProgress
-        this.btn_login.isEnabled = !inProgress
-        this.tv_register_now.isEnabled = !inProgress
-
-        this.tv_error_box.run {
-            if (this.visibility == View.VISIBLE && inProgress) {
-                this.visibility = View.INVISIBLE
-            }
-        }
     }
 }
